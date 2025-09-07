e54b14fbd7243aed63da74d16f874057507f9f41
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-07 17:31:00 +0530

    Added C program and sample output

--- DAY 16/mainQ32.c
+++ DAY 16/mainQ32.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 int main() 
 {
-  int n, rev = 0, r;
+  int n, ori, rev = 0, r;
   printf("Enter a number: ");
   scanf("%d", &n);
-  while (n = 0) 
+  ori = n;
+  while (n != 0) 
   {
     r = n % 10;
     rev = rev * 10 + r;
     n = n / 10;
   }
-  if (n == rev) 
+  if (ori == rev) 
   {
     printf("Palindrome");
   } 
