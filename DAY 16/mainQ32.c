2120ff9929704df6a51df416fe0afd59170c13b0
Anviksha <Anviksha.26976@stu.upes.ac.in>
2025-09-07 17:15:36 +0530

    mainQ32.c

---
+++ DAY 16/mainQ32.c
@@ -0,0 +1,24 @@
+//Write a program to check if a number is a palindrome.
+
+#include <stdio.h>
+int main() 
+{
+  int n, rev = 0, r;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  while (n = 0) 
+  {
+    r = n % 10;
+    rev = rev * 10 + r;
+    n = n / 10;
+  }
+  if (n == rev) 
+  {
+    printf("Palindrome");
+  } 
+  else 
+  {
+    printf("Not a palindrome");
+  }
+  return 0;
+}
